cf79f5f48cc8c08f5832a0d4cf48e4a220688a7d
Bryan <blinz117@gmail.com>
2024-06-10 12:54:51 -0500

    Add initial call to get classes list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import { CommonProviders } from "./components/providers/CommonProviders";
 import { ClassListSection } from "./features/class/ClassList";
 
 function App() {
   return (
-    <>
+    <CommonProviders>
       <h1>Dungeons and Dragons Character Guide</h1>
       <div className="p-4 border m-2">
         <ClassListSection />
       </div>
-    </>
+    </CommonProviders>
   );
 }
 
--- src/features/class/ClassList.tsx
+++ src/features/class/ClassList.tsx
@@ -1,31 +1,28 @@
-const mockClasses = [
-  "Barbarian",
-  "Bard",
-  "Cleric",
-  "Druid",
-  "Fighter",
-  "Monk",
-  "Paladin",
-  "Ranger",
-  "Rogue",
-  "Sorcerer",
-  "Warlock",
-  "Wizard",
-];
+import { useQuery } from "@tanstack/react-query";
+import { fetchClasses } from "./fetchClasses";
 
 export function ClassListSection() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["classes"],
+    queryFn: fetchClasses,
+  });
+
   return (
-    <>
+    <div>
       <h2>Classes</h2>
-      <ClassList />
-    </>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <ClassList classes={data.results.map((result) => result.name)} />
+      )}
+    </div>
   );
 }
 
-export function ClassList() {
+export function ClassList({ classes }: { classes: string[] }) {
   return (
     <ul>
-      {mockClasses.map((className) => (
+      {classes.map((className) => (
         <li key={className}>{className}</li>
       ))}
     </ul>
