1d8d7efe1d69b00e2c54a7e4d6bfb1703ee6b6da
Bryan <blinz117@gmail.com>
2024-06-11 18:43:50 -0500

    Simplify animation hierarchy

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -4,38 +4,29 @@ import loadingIcon from "@/assets/D20.svg";
 export function LoadingIndicator() {
   return (
     <div className="size-24">
-      <motion.div
+      <motion.img
+        src={loadingIcon}
         animate={{
           y: ["150%", 0],
           transition: {
-            duration: 0.5,
+            y: {
+              duration: 0.5,
+              repeat: Infinity,
+              ease: "circOut",
+              repeatType: "reverse",
+            },
+            duration: 1,
             repeat: Infinity,
-            ease: "circOut",
-            repeatType: "reverse",
+            repeatType: "mirror",
+            ease: "linear",
           },
+          opacity: 1,
+          rotate: 360,
         }}
-        exit={{ transition: { delay: 0.75 } }}
+        exit={{ opacity: 0, scale: 0, transition: { delay: 0.75 } }}
         key={"loading"}
         className="size-1/3 m-auto"
-      >
-        <motion.div
-          animate={{
-            opacity: 1,
-            rotate: 360,
-            scale: 1,
-            transition: {
-              duration: 1,
-              repeat: Infinity,
-              repeatType: "mirror",
-              ease: "linear",
-            },
-          }}
-          exit={{ opacity: 0, scale: 0, transition: { delay: 0.75 } }}
-          className="size-full"
-        >
-          <img src={loadingIcon} className="size-full" />
-        </motion.div>
-      </motion.div>
+      />
     </div>
   );
 }
