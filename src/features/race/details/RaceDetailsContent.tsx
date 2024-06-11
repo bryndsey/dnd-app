cf811a43f8a51320ad34aa848341107799328de8
Bryan <blinz117@gmail.com>
2024-06-11 12:40:19 -0500

    Extract component for consistency

---
+++ src/features/race/details/RaceDetailsContent.tsx
@@ -0,0 +1,28 @@
+import { RaceDetails } from "./RaceDetails";
+
+interface RaceDetailsContentProps {
+  data: RaceDetails;
+}
+
+export function RaceDetailsContent({ data }: RaceDetailsContentProps) {
+  return (
+    <>
+      <h2 className="text-2xl font-bold">{data.name}</h2>
+      <br />
+      <p>Age:</p>
+      <p>{data.age}</p>
+      <br />
+      <p>Language:</p>
+      <p>{data.language_desc}</p>
+      <br />
+      <p>Size:</p>
+      <p>{data.size_description}</p>
+      <br />
+      <p>Speed:</p>
+      <p>{`${data.speed} feet per round.`}</p>
+      <br />
+      <p>Alignment:</p>
+      <p>{data.alignment}</p>
+    </>
+  );
+}
--- src/features/race/details/RaceDetailsSection.tsx
+++ src/features/race/details/RaceDetailsSection.tsx
@@ -1,6 +1,6 @@
 import { useRace } from "./useRace";
-import { RaceDetails } from "./RaceDetails";
 import { ErrorDisplay } from "@/components/ErrorDisplay";
+import { RaceDetailsContent } from "./RaceDetailsContent";
 
 export interface RaceDetailsProps {
   index: string;
@@ -20,31 +20,4 @@ export function RaceDetailsSection({ index }: RaceDetailsProps) {
   }
 
   return <div>{<RaceDetailsContent data={result.data} />}</div>;
-}
-
-interface RaceDetailsContentProps {
-  data: RaceDetails;
-}
-
-export function RaceDetailsContent({ data }: RaceDetailsContentProps) {
-  return (
-    <>
-      <h2 className="text-2xl font-bold">{data.name}</h2>
-      <br />
-      <p>Age:</p>
-      <p>{data.age}</p>
-      <br />
-      <p>Language:</p>
-      <p>{data.language_desc}</p>
-      <br />
-      <p>Size:</p>
-      <p>{data.size_description}</p>
-      <br />
-      <p>Speed:</p>
-      <p>{`${data.speed} feet per round.`}</p>
-      <br />
-      <p>Alignment:</p>
-      <p>{data.alignment}</p>
-    </>
-  );
 }
