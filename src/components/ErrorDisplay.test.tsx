3cb8369f741df08a7bba6ad31dd54c58e10a5471
Bryan <blinz117@gmail.com>
2024-06-11 20:32:04 -0500

    Add more tests for error messages

--- src/components/ErrorDisplay.test.tsx
+++ src/components/ErrorDisplay.test.tsx
@@ -1,8 +1,29 @@
 import { render } from "@/test/utils";
 import "@testing-library/jest-dom";
+import { screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 import { axe } from "vitest-axe";
 import { ErrorDisplay } from "./ErrorDisplay";
+
+test("ErrorDisplay renders default error message", async () => {
+  render(<ErrorDisplay />);
+
+  await screen.findByRole("heading");
+  expect(screen.getByRole("heading")).toHaveTextContent(
+    "Oops, looks like we rolled a Nat 1..."
+  );
+  expect(screen.getByText("An error occurred")).toBeInTheDocument();
+});
+
+test("ErrorDisplay renders custom error message", async () => {
+  render(<ErrorDisplay message="Custom error message" />);
+
+  await screen.findByRole("heading");
+  expect(screen.getByRole("heading")).toHaveTextContent(
+    "Oops, looks like we rolled a Nat 1..."
+  );
+  expect(screen.getByText("Custom error message")).toBeInTheDocument();
+});
 
 test("ErrorDisplay has no automated a11y violations", async () => {
   const renderResult = render(<ErrorDisplay />);
