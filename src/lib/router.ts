bd125200f4c81eb2c7760a4b4e2731b7e35cd47a
Bryan <blinz117@gmail.com>
2024-06-12 03:12:00 -0500

    Update "Not found" page

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,10 +1,17 @@
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, NotFoundRoute } from "@tanstack/react-router";
+import { Route as rootRoute } from "@/routes/__root.tsx";
+import { ErrorDisplay } from "@/components/ErrorDisplay";
 
 // Import the generated route tree
 import { routeTree } from "@/routeTree.gen";
 
+const notFoundRoute = new NotFoundRoute({
+  getParentRoute: () => rootRoute,
+  component: () => ErrorDisplay({ message: "We couldn't find that page" }),
+});
+
 // Create a new router instance
-export const router = createRouter({ routeTree });
+export const router = createRouter({ routeTree, notFoundRoute });
 // Register the router instance for type safety
 
 declare module "@tanstack/react-router" {
