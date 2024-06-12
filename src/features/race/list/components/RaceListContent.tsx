5deb63b83b6bef12e89d270875da74b0f963dcce
Bryan <blinz117@gmail.com>
2024-06-12 02:22:04 -0500

    Go back to using standard links

--- src/features/race/list/components/RaceListContent.tsx
+++ src/features/race/list/components/RaceListContent.tsx
@@ -1,13 +1,13 @@
 import { Race } from "../types/Race";
-import { Link } from "@/components/Link";
+import { Link } from "@tanstack/react-router";
 
 export function RaceListContent({ races }: { races: Race[] }) {
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {races.map(({ index, name }) => (
         <li key={index}>
-          <Link href={`/race/${index}`}>
-            <div className="rounded-xl shadow-md p-6 sm:p-8 border text-xl font-bold">
+          <Link to={`/race/${index}`}>
+            <div className="rounded-xl shadow-md p-6 sm:p-8 border text-xl font-bold hover:-translate-y-1 transition-all hover:shadow-lg active:translate-y-0 active:shadow-md">
               {name}
             </div>
           </Link>
