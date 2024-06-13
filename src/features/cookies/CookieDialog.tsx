b3b7ce97b1485d889fc6eba0ad0a81c9c221cfc9
Bryan <blinz117@gmail.com>
2024-06-13 11:22:49 -0500

    Handle persisted state

--- src/features/cookies/CookieDialog.tsx
+++ src/features/cookies/CookieDialog.tsx
@@ -1,22 +1,79 @@
 import { Popover } from "@/components/Popover";
 import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
+import { useState } from "react";
+
+const hasSubmittedCookiePreference = atomWithStorage(
+  "hasSubmittedCookiePreference",
+  false,
+  undefined,
+  { getOnInit: true },
+);
+const cookiePreference = atomWithStorage("cookiePreference", true, undefined, {
+  getOnInit: true,
+});
 
 export function CookieDialog() {
+  const [submitted, setSubmitted] = useAtom(hasSubmittedCookiePreference);
+  const [isOpen, setIsOpen] = useState(!submitted);
+  const [savedPreference, setSavedPreference] = useAtom(cookiePreference);
+  const [isChecked, setIsChecked] = useState(savedPreference);
+
   return (
-    <Popover>
-      <PopoverTrigger>Cookies</PopoverTrigger>
+    <ControlledCookieDialog
+      isOpen={isOpen}
+      isChecked={isChecked}
+      onCheckedChange={() => setIsChecked(!isChecked)}
+      onSubmit={() => {
+        setSavedPreference(isChecked);
+        setSubmitted(true);
+        setIsOpen(false);
+      }}
+      onTriggerClicked={() => {
+        setIsOpen(!isOpen);
+      }}
+    />
+  );
+}
+
+interface ControlledCookieDialogProps {
+  isOpen: boolean;
+  isChecked: boolean;
+  onCheckedChange: () => void;
+  onSubmit: () => void;
+  onTriggerClicked: () => void;
+}
+
+function ControlledCookieDialog({
+  isOpen,
+  isChecked,
+  onCheckedChange,
+  onSubmit,
+  onTriggerClicked,
+}: ControlledCookieDialogProps) {
+  return (
+    <Popover open={isOpen}>
+      <PopoverTrigger onClick={onTriggerClicked}>Cookies</PopoverTrigger>
       <PopoverContent>
         <div className="m-4 max-w-sm rounded-lg border p-6 shadow-xl">
-          <p className="font-bold">This is a fake cookies popup.</p>
+          <p className="font-bold">This is a fake cookies popup</p>
           <p>
             This website doesn't store any cookies. I just want to know if you
             like to eat cookies?
           </p>
           <div className="space-x-2">
-            <input type="checkbox" className="mt-4" />
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={onCheckedChange}
+            />
             <label>Cookies... yum!</label>
           </div>
-          <button className="mt-4 rounded bg-blue-500 p-2 text-white transition-all hover:-translate-y-1 active:translate-y-0">
+          <button
+            onClick={onSubmit}
+            className="mt-4 rounded bg-blue-500 p-2 text-white transition-all hover:-translate-y-1 active:translate-y-0"
+          >
             Submit
           </button>
         </div>
